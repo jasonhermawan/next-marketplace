3d0f44c3652a59823c58c76f430b74fc6bee8993
jasonhermawan <jasonhermawans@gmail.com>
2023-12-05 19:45:04 +0700

    connect products api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,30 @@
 import CarouselComponent from "@/components/Carousel";
 import ProductCard from "@/components/ProductCard";
+import axios from "axios";
 
-export default function Home() {
+export default async function Home() {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/product`)
+  const printProducts = () => {
+    return response.data.map((val) => {
+      return (
+        <ProductCard
+          key={val.id}
+          image={`${process.env.NEXT_PUBLIC_API_BASE_URL}/public/products/${val.product_images[0].image}`}
+          price={val.normalPrice}
+          name={val.name}
+        />
+      )
+    })
+  }
+
+
   return (
     <div>
       <CarouselComponent />
       <div className="mt-12">
         <h1 className="text-4xl font-semibold tracking-tight mb-5">Best Deals</h1>
         <div className="flex">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {printProducts()}
         </div>
       </div>
     </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = (props) => {
   return (
     <div className='w-1/6 p-2'>
       <div className='border border-neutral-100 rounded-md'>
-        <img className='w-full rounded-t-md' src="https://thumbor.sirclocdn.com/unsafe/186x186/filters:format(webp):quality(70)/web.mitra10.com/media/catalog/product/1/1/1100007443_website_2.jpg" alt="" />
+        <img src={props.image} className='w-full rounded-t-md' alt={props.name} />
         <div className='p-2'>
-          <h3 className='text-md font-semibold text-red-500'>IDR 2,410,073</h3>
-          <h1 className='text-md font-semibold mt-2 line-clamp-2'>Sharp Led TV 2T-C32Df1I 32 Inch Hitam</h1>
+          <h3 className='text-md font-semibold text-red-500'>Rp {(props.price).toLocaleString("id")}</h3>
+          <h1 className='text-md font-semibold mt-2 line-clamp-2'>{props.name}</h1>
         </div>
         <div className='p-2 flex items-center justify-between'>
           <div className='flex gap-0.5'>
-            <i class='bx bxs-star text-md text-blue-800' ></i>
-            <i class='bx bxs-star text-md text-blue-800' ></i>
-            <i class='bx bxs-star text-md text-blue-800' ></i>
-            <i class='bx bxs-star text-md text-blue-800' ></i>
-            <i class='bx bxs-star text-md text-blue-800' ></i>
+            <i className='bx bxs-star text-md text-blue-800' ></i>
+            <i className='bx bxs-star text-md text-blue-800' ></i>
+            <i className='bx bxs-star text-md text-blue-800' ></i>
+            <i className='bx bxs-star text-md text-blue-800' ></i>
+            <i className='bx bxs-star text-md text-blue-800' ></i>
           </div>
           <i className='bx bx-heart text-xl cursor-pointer' />
         </div>
